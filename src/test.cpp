dc891326c8e52a12fb77033f9c7b8c6c87547540
Melecio Ponte <pontezambrano@gmail.com>
2014-04-22 15:46:55 -0430

    Cambio nombre de rank a top Se imprime rank generado a arreglos rankeados

--- src/test.cpp
+++ src/test.cpp
@@ -4,32 +4,32 @@
 void array_swap(int &a, int &b);
 void print_array(int *array, int n);
 int rank(int n, int *array);
-int aux_rank(int n, int *array, int *inverse);
-int* inv_array(int* array, int size);
 int* unrank(int n, int r);
+int* inv_array(int* array, int size);
 
 int main(int argc, char const *argv[]) {
     int n;
-    int rank;
+    int top;
     int *array;
 
     if (argc == 3) {
         n = atoi(argv[1]);
-        rank = atoi(argv[2]);
+        top = atoi(argv[2]);
     } else if (argc == 2) {
         n = atoi(argv[1]);
-        rank = factorial(n) - 1;
+        top = factorial(n) - 1;
     } else {
         n = 8;
-        rank = factorial(n) - 1;
+        top = factorial(n) - 1;
     }
 
-    std::cout << "n: (" << n << ") rank: (" << rank << ")\n";
+    std::cout << "n: (" << n << ") top: (" << top << ")\n";
 
-    for (int i = 0; i <= rank; ++i) {
+    for (int i = 0; i <= top; ++i) {
         std::cout << i << ": ";
         array = unrank(n , i);
         print_array(array, n);
+        std::cout << " " << rank(n, array) << "\n";
         delete[] array;
     }
 
@@ -47,10 +47,16 @@ void print_array(int *array, int n) {
     for (unsigned i = 0; i < n; ++i) {
         std::cout << array[i] << " ";
     }
-    std::cout << std::endl;
 }
 
 ////////////////////////////////////////
+
+void aux_unrank(int n, int r, int *array) {
+    if (n > 0) {
+        array_swap(array[n-1], array[r % n]);
+        aux_unrank(n-1, floor(r/n), array);
+    }
+}
 
 int *unrank(int n, int r) {
     int *array = new int[n];
@@ -65,20 +71,7 @@ int *unrank(int n, int r) {
     return array;
 }
 
-void aux_unrank(int n, int r, int *array) {
-    if (n > 0) {
-        array_swap(array[n-1], array[r % n]);
-        aux_unrank(n-1, floor(r/n), array);
-    }
-}
-
 ////////////////////////////////////////
-
-int rank(int n, int *array) {
-    int *inverse = inv_array(array, n);
-
-    return aux_rank(n, array, inverse);
-}
 
 int aux_rank(int n, int *array, int *inverse) {
     if (n == 1) {
@@ -90,6 +83,12 @@ int aux_rank(int n, int *array, int *inverse) {
     array_swap(inverse[s], inverse[n-1]);
 
     return s+n*aux_rank(n-1, array, inverse);
+}
+
+int rank(int n, int *array) {
+    int *inverse = inv_array(array, n);
+
+    return aux_rank(n, array, inverse);
 }
 
 ////////////////////////////////////////
