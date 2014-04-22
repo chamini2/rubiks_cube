e8b1c7b7fefe1b433d6d8166cec1b60d909874e5
Melecio Ponte <pontezambrano@gmail.com>
2014-04-22 14:40:32 -0430

    Se implementa unrank y asociados

--- src/test.cpp
+++ src/test.cpp
@@ -1,15 +1,47 @@
 #include "HashTable.hpp"
+#include <math.h>
+
+void array_swap(int &a, int &b);
+void print_array(int *array, int n);
+void unrank(int n, int r, int *array);
 
 int main(int argc, char const *argv[]) {
-    Cube* cube = new Cube(false);
-    Cube* a = new Cube(true);
-    // std::queue<Cube*> q = cube->succ_corners();
-    // a = q.front();
+    int n = atoi(argv[1]);
+    int rank = atoi(argv[2]);
+    int *array = new int[n];
+    int *array2 = new int[n];
 
-    if (cube->equals(a)) {
-        std::cout << "BIEN";
-    } else {
-        std::cout << "MAL";
+    //  Arreglo identidad
+    for (unsigned i = 0; i < n; ++i) {
+        array[i] = i;
+        array2[i] = i;
     }
 
+    std::cout << rank << ": ";
+    unrank(n , rank, array2);
+    print_array(array2, n);
+
+    return 0;
+}
+
+
+void array_swap(int &a, int &b) {
+    int temp;
+    temp = b;
+    b = a;
+    a = temp;
+}
+
+void print_array(int *array, int n) {
+    for (unsigned i = 0; i < n; ++i) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void unrank(int n, int r, int *array) {
+    if (n > 0) {
+        array_swap(array[n-1], array[r % n]);
+        unrank(n-1, floor(r/n), array);
+    }
 }
