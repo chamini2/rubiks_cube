6854be46205dcaa30b2b1252298d97af3169ebb5
Matteo Ferrando <matteo.ferrando2@gmail.com>
2014-04-22 22:09:16 -0430

    leak al no liberar el array pasado a rank

--- src/rank.cpp
+++ src/rank.cpp
@@ -11,7 +11,7 @@ void array_swap(int &a, int &b) {
 int* inv_array(int* array, int size) {
     int *inverse = new int[size];
 
-    for (unsigned i = 0; i < size; ++i) {
+    for (int i = 0; i < size; ++i) {
         inverse[array[i]] = i;
     }
 
@@ -27,15 +27,15 @@ void aux_unrank(int n, int r, int *array) {
     }
 }
 
-int *unrank(int n, int r) {
+int *unrank( intsize, int r) {
     int *array = new int[n];
 
     //  Arreglo identidad
-    for (unsigned i = 0; i < n; ++i) {
+    for (int i = 0; i < size; ++i) {
         array[i] = i;
     }
 
-    aux_unrank(n, r, array);
+    aux_unrank(size, r, array);
 
     return array;
 }
@@ -54,26 +54,28 @@ int aux_rank(int n, int *array, int *inverse) {
     return s + n * aux_rank(n-1, array, inverse);
 }
 
-int rank(int n, int *array) {
+int rank(int size, int *array) {
     int *inverse;
-    int *aux = new int[n];
+    int *aux = new int[size];
     int value, accum = 0;
 
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < size; ++i) {
         aux[i] = cubie_to_pos(array[i]);
     }
 
-    inverse = inv_array(aux, n);
-    value = aux_rank(n, aux, inverse);
+    inverse = inv_array(aux, size);
+    value = aux_rank(size, aux, inverse);
 
     delete[] inverse;
     delete[] aux;
 
     value *= pow(3,8);
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < size; ++i) {
         accum = accum * 3 + cubie_to_orien(array[i]);
     }
     value += accum;
+
+    delete array;
 
     return value;
 }
